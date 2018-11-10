c2c88ead96c127b01feaa28efb31f2c4c9cfd515
Fatih Balcı <fbalci@gmail.com>
2018-11-10 10:33:31 +0300

    cosmetic changes in code

--- proIMP/frmPreferences.cs
+++ proIMP/frmPreferences.cs
@@ -110,88 +110,41 @@ namespace proIMP {
 
                     SQLiteCommand dbCommand = new SQLiteCommand( frmMain.sqlCon );
                     try {
+                        List<string> sql = new List<string>() {
+                            "CREATE TABLE category (category_id INTEGER PRIMARY KEY AUTOINCREMENT, category_name VARCHAR (48) UNIQUE ON CONFLICT IGNORE, category_desc TEXT)",
+                            "CREATE INDEX category_id ON category (category_id)",
+                            "CREATE TABLE customer (customer_id INTEGER PRIMARY KEY AUTOINCREMENT, customer_name VARCHAR (48) UNIQUE ON CONFLICT IGNORE, customer_desc TEXT)",
+                            "CREATE INDEX customer_id ON customer (customer_id)",
+                            "CREATE TABLE image (image_id INTEGER PRIMARY KEY AUTOINCREMENT, image_md5 VARCHAR (32) UNIQUE ON CONFLICT IGNORE, image_binary BLOB)",
+                            "CREATE INDEX image_id ON image (image_id)",
+                            "CREATE INDEX image_md5 ON image (image_md5)",
+                            "CREATE TABLE product (product_id INTEGER PRIMARY KEY AUTOINCREMENT, product_name VARCHAR (64) UNIQUE ON CONFLICT IGNORE, product_catid INTEGER, product_desc TEXT, product_unit VARCHAR (3), product_barcode VARCHAR (32), product_imageid VARCHAR (32))",
+                            "CREATE INDEX product_id ON product (product_id)",
+                            "CREATE INDEX product_catid ON product (product_catid)",
+                            "CREATE INDEX product_imageid ON product (product_imageid)",
+                            "CREATE TABLE stock_flow (sflow_id INTEGER PRIMARY KEY AUTOINCREMENT, sflow_sid INTEGER, sflow_productid INTEGER, sflow_quantity DOUBLE, sflow_price DOUBLE)",
+                            "CREATE INDEX sflow_id ON stock_flow (sflow_id)",
+                            "CREATE INDEX sflow_sid ON stock_flow (sflow_sid)",
+                            "CREATE INDEX sflow_productid ON stock_flow (sflow_productid)",
+                            "CREATE TABLE stock (stock_id INTEGER PRIMARY KEY AUTOINCREMENT, stock_type INT, stock_date DATETIME, stock_supplier INTEGER, stock_desc TEXT)",
+                            "CREATE INDEX stock_id ON stock (stock_id)",
+                            "CREATE INDEX stock_supplier ON stock (stock_supplier)",
+                            "CREATE TRIGGER stock_afterdelete AFTER DELETE ON stock FOR EACH ROW BEGIN DELETE FROM stock_flow WHERE sflow_sid = OLD.stock_id; END",
+                            "CREATE TABLE supplier (supplier_id INTEGER PRIMARY KEY AUTOINCREMENT, supplier_name VARCHAR (48) UNIQUE ON CONFLICT IGNORE, supplier_desc TEXT)",
+                            "CREATE INDEX supplier_id ON supplier (supplier_id)",
+                            "CREATE TABLE warehouse (warehouse_id INTEGER PRIMARY KEY ASC AUTOINCREMENT, warehouse_name VARCHAR (48) UNIQUE ON CONFLICT IGNORE, warehouse_desc TEXT)",
+                            "CREATE INDEX warehouse_id ON warehouse (warehouse_id)",
+                            "CREATE VIEW product_list AS SELECT t1.product_id, t1.product_name, t2.category_name, CASE WHEN t3.product_count IS NULL THEN NULL ELSE t3.product_count END AS product_count, CASE WHEN t4.product_price IS NULL THEN NULL ELSE t4.product_price END AS sflow_price FROM product AS t1 LEFT JOIN category AS t2 ON t1.product_catid = t2.category_id LEFT JOIN( SELECT sflow_productid, SUM(sflow_quantity) AS product_count FROM stock_flow GROUP BY sflow_productid ) AS t3 ON t1.product_id = t3.sflow_productid LEFT JOIN( SELECT t1.sflow_productid, MAX(t1.sflow_price) AS product_price FROM stock_flow AS t1 INNER JOIN stock AS t2 ON t1.sflow_sid = t2.stock_id INNER JOIN (SELECT t1.sflow_productid, MAX(t2.stock_date) AS stock_date FROM stock_flow AS t1 INNER JOIN stock AS t2 ON t1.sflow_sid = t2.stock_id WHERE t2.stock_type = 1 GROUP BY t1.sflow_productid) AS t3 ON t1.sflow_productid = t3.sflow_productid AND t2.stock_date = t3.stock_date WHERE t2.stock_type = 1 GROUP BY t1.sflow_productid) AS t4 ON t1.product_id = t4.sflow_productid",
+                            "CREATE VIEW stockflow_list AS SELECT t1.stock_id, t1.stock_type, t1.stock_date, CASE WHEN t2.supplier_id IS NULL THEN t3.customer_name ELSE t2.supplier_name END AS stock_name, t1.stock_desc, CASE WHEN t3.product_price IS NULL THEN 0 ELSE t3.product_price END AS product_price, CASE WHEN t3.product_count IS NULL THEN 0 ELSE t3.product_count END AS product_count, CASE WHEN t3.product_sum IS NULL THEN 0 ELSE CASE WHEN t1.stock_type = 1 THEN t3.product_sum ELSE ABS( t3.product_sum ) END END AS product_sum FROM stock AS t1 LEFT JOIN supplier AS t2 ON t1.stock_supplier = t2.supplier_id AND t1.stock_type = 1 LEFT JOIN customer AS t3 ON t1.stock_supplier = t3.customer_id AND t1.stock_type = 2 LEFT JOIN( SELECT sflow_sid AS sflow_sid, COUNT(1) AS product_count, SUM(sflow_quantity) AS product_sum, SUM(sflow_quantity * sflow_price) AS product_price FROM stock_flow GROUP BY sflow_sid) AS t3 ON t1.stock_id = t3.sflow_sid",
+                            "CREATE VIEW report_product_count AS SELECT t2.product_id, t2.product_name, t4.category_name, SUM(CASE WHEN t5.stock_type = 1 THEN t1.sflow_quantity ELSE 0 END) AS product_quantity_in, SUM(CASE WHEN t5.stock_type = 2 THEN t1.sflow_quantity ELSE 0 END) AS product_quantity_out FROM stock_flow AS t1 INNER JOIN product AS t2 ON t2.product_id = t1.sflow_productid INNER JOIN warehouse AS t3 ON t1.sflow_warehouseid = t3.warehouse_id INNER JOIN category AS t4 ON t2.product_catid = t4.category_id INNER JOIN stock AS t5 ON t1.sflow_sid = t5.stock_id GROUP BY t2.product_id, t2.product_name, t4.category_name",
+                            "CREATE VIEW report_product_flow AS SELECT t4.stock_date, t2.product_id, t2.product_name, t2.product_unit, t3.category_id, t3.category_name, t1.sflow_warehouseid AS warehouse_id, CASE WHEN t4.stock_type = 1 THEN t1.sflow_quantity ELSE 0 END AS product_quantity_in, CASE WHEN t4.stock_type = 2 THEN t1.sflow_quantity ELSE 0 END AS product_quantity_out FROM stock_flow AS t1 INNER JOIN product AS t2 ON t2.product_id = t1.sflow_productid INNER JOIN category AS t3 ON t2.product_catid = t3.category_id INNER JOIN stock AS t4 ON t1.sflow_sid = t4.stock_id"
+                        };
+
                         dbCommand.Transaction = frmMain.sqlCon.BeginTransaction();
-
-                        dbCommand.CommandText = "CREATE TABLE category (category_id INTEGER PRIMARY KEY AUTOINCREMENT, category_name VARCHAR (48) UNIQUE ON CONFLICT IGNORE, category_desc TEXT)";
-                        dbCommand.ExecuteNonQuery();
-
-                        dbCommand.CommandText = "CREATE INDEX category_id ON category (category_id)";
-                        dbCommand.ExecuteNonQuery();
-
-                        dbCommand.CommandText = "CREATE TABLE customer (customer_id INTEGER PRIMARY KEY AUTOINCREMENT, customer_name VARCHAR (48) UNIQUE ON CONFLICT IGNORE, customer_desc TEXT)";
-                        dbCommand.ExecuteNonQuery();
-
-                        dbCommand.CommandText = "CREATE INDEX customer_id ON customer (customer_id)";
-                        dbCommand.ExecuteNonQuery();
-
-                        dbCommand.CommandText = "CREATE TABLE image (image_id INTEGER PRIMARY KEY AUTOINCREMENT, image_md5 VARCHAR (32) UNIQUE ON CONFLICT IGNORE, image_binary BLOB)";
-                        dbCommand.ExecuteNonQuery();
-
-                        dbCommand.CommandText = "CREATE INDEX image_id ON image (image_id)";
-                        dbCommand.ExecuteNonQuery();
-
-                        dbCommand.CommandText = "CREATE INDEX image_md5 ON image (image_md5)";
-                        dbCommand.ExecuteNonQuery();
-
-                        dbCommand.CommandText = "CREATE TABLE product (product_id INTEGER PRIMARY KEY AUTOINCREMENT, product_name VARCHAR (64) UNIQUE ON CONFLICT IGNORE, product_catid INTEGER, product_desc TEXT, product_unit VARCHAR (3), product_barcode VARCHAR (32), product_imageid VARCHAR (32))";
-                        dbCommand.ExecuteNonQuery();
-
-                        dbCommand.CommandText = "CREATE INDEX product_id ON product (product_id)";
-                        dbCommand.ExecuteNonQuery();
-
-                        dbCommand.CommandText = "CREATE INDEX product_catid ON product (product_catid)";
-                        dbCommand.ExecuteNonQuery();
-
-                        dbCommand.CommandText = "CREATE INDEX product_imageid ON product (product_imageid)";
-                        dbCommand.ExecuteNonQuery();
-
-                        dbCommand.CommandText = "CREATE TABLE stock_flow (sflow_id INTEGER PRIMARY KEY AUTOINCREMENT, sflow_sid INTEGER, sflow_productid INTEGER, sflow_quantity DOUBLE, sflow_price DOUBLE)";
-                        dbCommand.ExecuteNonQuery();
-
-                        dbCommand.CommandText = "CREATE INDEX sflow_id ON stock_flow (sflow_id)";
-                        dbCommand.ExecuteNonQuery();
-
-                        dbCommand.CommandText = "CREATE INDEX sflow_sid ON stock_flow (sflow_sid)";
-                        dbCommand.ExecuteNonQuery();
-
-                        dbCommand.CommandText = "CREATE INDEX sflow_productid ON stock_flow (sflow_productid)";
-                        dbCommand.ExecuteNonQuery();
-
-                        dbCommand.CommandText = "CREATE TABLE stock (stock_id INTEGER PRIMARY KEY AUTOINCREMENT, stock_type INT, stock_date DATETIME, stock_supplier INTEGER, stock_desc TEXT)";
-                        dbCommand.ExecuteNonQuery();
-
-                        dbCommand.CommandText = "CREATE INDEX stock_id ON stock (stock_id)";
-                        dbCommand.ExecuteNonQuery();
-
-                        dbCommand.CommandText = "CREATE INDEX stock_supplier ON stock (stock_supplier)";
-                        dbCommand.ExecuteNonQuery();
-
-                        dbCommand.CommandText = "CREATE TRIGGER stock_afterdelete AFTER DELETE ON stock FOR EACH ROW BEGIN DELETE FROM stock_flow WHERE sflow_sid = OLD.stock_id; END";
-                        dbCommand.ExecuteNonQuery();
-
-                        dbCommand.CommandText = "CREATE TABLE supplier (supplier_id INTEGER PRIMARY KEY AUTOINCREMENT, supplier_name VARCHAR (48) UNIQUE ON CONFLICT IGNORE, supplier_desc TEXT)";
-                        dbCommand.ExecuteNonQuery();
-
-                        dbCommand.CommandText = "CREATE INDEX supplier_id ON supplier (supplier_id)";
-                        dbCommand.ExecuteNonQuery();
-
-                        dbCommand.CommandText = "CREATE TABLE warehouse (warehouse_id INTEGER PRIMARY KEY ASC AUTOINCREMENT, warehouse_name VARCHAR (48) UNIQUE ON CONFLICT IGNORE, warehouse_desc TEXT)";
-                        dbCommand.ExecuteNonQuery();
-
-                        dbCommand.CommandText = "CREATE INDEX warehouse_id ON warehouse (warehouse_id)";
-                        dbCommand.ExecuteNonQuery();
-
-                        dbCommand.CommandText = "CREATE VIEW product_list AS SELECT t1.product_id, t1.product_name, t2.category_name, CASE WHEN t3.product_count IS NULL THEN NULL ELSE t3.product_count END AS product_count, CASE WHEN t4.product_price IS NULL THEN NULL ELSE t4.product_price END AS sflow_price FROM product AS t1 LEFT JOIN category AS t2 ON t1.product_catid = t2.category_id LEFT JOIN( SELECT sflow_productid, SUM(sflow_quantity) AS product_count FROM stock_flow GROUP BY sflow_productid ) AS t3 ON t1.product_id = t3.sflow_productid LEFT JOIN( SELECT t1.sflow_productid, MAX(t1.sflow_price) AS product_price FROM stock_flow AS t1 INNER JOIN stock AS t2 ON t1.sflow_sid = t2.stock_id INNER JOIN (SELECT t1.sflow_productid, MAX(t2.stock_date) AS stock_date FROM stock_flow AS t1 INNER JOIN stock AS t2 ON t1.sflow_sid = t2.stock_id WHERE t2.stock_type = 1 GROUP BY t1.sflow_productid) AS t3 ON t1.sflow_productid = t3.sflow_productid AND t2.stock_date = t3.stock_date WHERE t2.stock_type = 1 GROUP BY t1.sflow_productid) AS t4 ON t1.product_id = t4.sflow_productid";
-                        dbCommand.ExecuteNonQuery();
-
-                        dbCommand.CommandText = "CREATE VIEW stockflow_list AS SELECT t1.stock_id, t1.stock_type, t1.stock_date, CASE WHEN t2.supplier_id IS NULL THEN t3.customer_name ELSE t2.supplier_name END AS stock_name, t1.stock_desc, CASE WHEN t3.product_price IS NULL THEN 0 ELSE t3.product_price END AS product_price, CASE WHEN t3.product_count IS NULL THEN 0 ELSE t3.product_count END AS product_count, CASE WHEN t3.product_sum IS NULL THEN 0 ELSE CASE WHEN t1.stock_type = 1 THEN t3.product_sum ELSE ABS( t3.product_sum ) END END AS product_sum FROM stock AS t1 LEFT JOIN supplier AS t2 ON t1.stock_supplier = t2.supplier_id AND t1.stock_type = 1 LEFT JOIN customer AS t3 ON t1.stock_supplier = t3.customer_id AND t1.stock_type = 2 LEFT JOIN( SELECT sflow_sid AS sflow_sid, COUNT(1) AS product_count, SUM(sflow_quantity) AS product_sum, SUM(sflow_quantity * sflow_price) AS product_price FROM stock_flow GROUP BY sflow_sid) AS t3 ON t1.stock_id = t3.sflow_sid";
-                        dbCommand.ExecuteNonQuery();
-
-                        dbCommand.CommandText = "CREATE VIEW report_product_count AS SELECT t2.product_id, t2.product_name, t4.category_name, SUM(CASE WHEN t5.stock_type = 1 THEN t1.sflow_quantity ELSE 0 END) AS product_quantity_in, SUM(CASE WHEN t5.stock_type = 2 THEN t1.sflow_quantity ELSE 0 END) AS product_quantity_out FROM stock_flow AS t1 INNER JOIN product AS t2 ON t2.product_id = t1.sflow_productid INNER JOIN warehouse AS t3 ON t1.sflow_warehouseid = t3.warehouse_id INNER JOIN category AS t4 ON t2.product_catid = t4.category_id INNER JOIN stock AS t5 ON t1.sflow_sid = t5.stock_id GROUP BY t2.product_id, t2.product_name, t4.category_name";
-                        dbCommand.ExecuteNonQuery();
-
-                        dbCommand.CommandText = "CREATE VIEW report_product_flow AS SELECT t4.stock_date, t2.product_id, t2.product_name, t2.product_unit, t3.category_id, t3.category_name, t1.sflow_warehouseid AS warehouse_id, CASE WHEN t4.stock_type = 1 THEN t1.sflow_quantity ELSE 0 END AS product_quantity_in, CASE WHEN t4.stock_type = 2 THEN t1.sflow_quantity ELSE 0 END AS product_quantity_out FROM stock_flow AS t1 INNER JOIN product AS t2 ON t2.product_id = t1.sflow_productid INNER JOIN category AS t3 ON t2.product_catid = t3.category_id INNER JOIN stock AS t4 ON t1.sflow_sid = t4.stock_id";
-                        dbCommand.ExecuteNonQuery();
+                        foreach(string query in sql) {
+                            dbCommand.CommandText = query;
+                            dbCommand.ExecuteNonQuery();
+                        }
 
                         dbCommand.Transaction.Commit();
 
